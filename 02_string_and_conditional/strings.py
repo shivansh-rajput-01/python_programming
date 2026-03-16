000d92b826350268a29bb3ac0bcdb6f4e4f7844c
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-03-16 14:41:04 +0530

    learned strings in python and ways to create them

---
+++ 02_string_and_conditional/strings.py
@@ -0,0 +1,16 @@
+# can be created in '' , "", and """"""
+# these can be used when using apostophe in string
+# triple quotes created multiline strings
+
+str1 = 'hello'
+str2 = "hello"
+str3 = """this is
+a multiline string
+"""
+str4 = "this is a\
+    multiline string" # does not reserve a new line
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
