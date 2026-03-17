98e17cae8cbd2e954f6c888351d59a266774999c
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-03-17 21:32:26 +0530

    implemented lists in python

---
+++ 03_list_and_tuples/lists.py
@@ -0,0 +1,13 @@
+# list are mutable data structures in python
+# they are written in []
+# it can contain values of different data types
+
+list1 = [1, 2, 3, 4, 5]
+
+list2 = ["shinchan", "doraemon"]
+
+list3 = ["shivansh", 33, 90]
+
+print(list1)
+print(list2)
+print(list3)
