8603ee8f132078f714e30273dc823f5f14ab11c4
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-03-17 21:37:51 +0530

    implemented input taking and adding to list practice question

---
+++ 03_practice_set/list_input.py
@@ -0,0 +1,8 @@
+list1 = []
+name = input("Enter name of your favourite food: ")
+list1.append(name)
+name = input("Enter name of your favourite food: ")
+list1.append(name)
+name = input("Enter name of your favourite food: ")
+list1.append(name)
+print(list1)
