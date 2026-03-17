46ce13e2d42d2e8f3dcf36b4308d5d8851142ecb
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-03-17 21:39:21 +0530

    implemented program to find number of students with grade 'A' and sort the grade list

---
+++ 03_practice_set/grade_count.py
@@ -0,0 +1,9 @@
+# WAP to count the number of students with grade 'A'
+tuple1 = ('A', 'B', 'A', 'C', 'D')
+
+print(tuple1.count('A'))
+
+# store this in a list and sort from 'A' to 'D'
+list1 = list(tuple1)
+list1.sort()
+print(list1)
