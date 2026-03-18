d283acead0d50062318668ec3e76058732c1b382
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-03-18 21:27:13 +0530

    implement practice question to store word and their meanings in dictionary

---
+++ 04_practice_set/word_meanings.py
@@ -0,0 +1,11 @@
+# store the following word meanings in a python dictionary
+
+# table : "a piece of furniture", "list of facts and figures"
+# cat: "a small animal"
+
+dict1 = {
+    "table" : ["a piece of furniture", "list of facts and figures"],
+    "cat" : ["a small animal"]
+}
+
+print(dict1)
